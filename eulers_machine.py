085a058470a41c3b6eb2ad4181d1c1e5be27c511
hugh-dickens <73748574+hugh-dickens@users.noreply.github.com>
2022-08-25 16:58:52 +0100

    Add files via upload

--- eulers_machine.py
+++ eulers_machine.py
@@ -1,8 +1,17 @@
 class EulersMachine:
-    def __init__(self):
-        
+    def __init__(self, input_number):
+        self.input_number = input_number
+        self.output_number = 0
+
+    def calculate_output_number(self):
+        self.output_number = self.input_number - 1
+
+    def print_output_number(self):
+        print("Output number for this case is: {} ".format(self.output_number))
 
 if __name__ == '__main__':
 
-    my_eulers_machine = EulersMachine()
-    print("Welcome to Eulers Machine calculator")
+    print("Welcome to Eulers Machine calculator. Input a number: ")
+    my_eulers_machine = EulersMachine(5)
+
+    
